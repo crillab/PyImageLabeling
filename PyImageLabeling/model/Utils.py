b8b2fe3748756220604ea9a2bbe90b2eaaf674c6
pimfayette-lab <pimfayette@laposte.net>
2026-01-19 09:41:06 +0100

    build exe more flexible

--- PyImageLabeling/model/Utils.py
+++ PyImageLabeling/model/Utils.py
@@ -48,7 +48,7 @@ import shutil
 
 class Utils:
 
-    __version__ = "1.0.5"
+    __version__ = "1.0.6"
 
     @staticmethod
     def get_base_dir():
--- build-exec/build_exe.py
+++ build-exec/build_exe.py
@@ -1,4 +1,5 @@
 import os
+import platform
 from PyImageLabeling.model.Utils import Utils
 
 print("Do you want to update the version? (Y/N): ", end="")
@@ -18,15 +19,20 @@ input()
 version = Utils.get_version()
 exe_name = f"PyImageLabeling_v{version}"
 
+# Use the correct separator based on the OS
+separator = ';' if platform.system() == 'Windows' else ':'
+
 command = (
     f'pyinstaller --onefile --noconsole '
     f'--name "{exe_name}" '
-    f'--add-data "PyImageLabeling/config.json;." '
-    f'--add-data "PyImageLabeling/default_parameters.json;." '
-    f'--add-data "PyImageLabeling/style.css;." '
-    f'--add-data "PyImageLabeling/icons/*.png;icons" '
+    f'--add-data "PyImageLabeling/config.json{separator}." '
+    f'--add-data "PyImageLabeling/default_parameters.json{separator}." '
+    f'--add-data "PyImageLabeling/style.css{separator}." '
+    f'--add-data "PyImageLabeling/icons/*.png{separator}icons" '
     f'--icon "PyImageLabeling/icons/maia3.ico" '
+    f'--strip '  
     f'PyImageLabeling/__main__.py'
 )
 
+print(f"\nExecuting: {command}\n")
 os.system(command)
